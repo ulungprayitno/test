f8d0eef86f84ace17609f347c281a718666ca4ae
ulungp <ulung.prayitno93@gmail.com>
2021-10-28 11:33:39 +0700

    result test no 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // const findElement = (arr) => {
+  //   let idx = -1
+  //   let set = new Set()
+  //   arr.map((item, i) => {
+  //     if(set.has(item)) idx = i
+  //     else set.add(item)
+
+  //     return idx
+  //   })
+  // }
+
+  const names = [
+    {id: 1, name: "Agung"},
+    {id: 2, name: "Boby"},
+    {id: 3, name: "Danu"},
+    {id: 4, name: "Cika"},
+    {id: 5, name: "Andre"},
+  ]
+
+  const [account, setAccount] = useState(names)
+
+  const searchAccount = (value) => {
+    const ressult = names.filter(n => n.name.includes(value))
+    setAccount(ressult)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginTop: '4em'}}>
+      
+      <form>
+        <input type='text' onChange={(e) => searchAccount(e.target.value)} placeholder='search' />
+      </form>
+
+      <div style={{paddingTop : '2em'}}>
+        { account.map(item => (
+          <div key={item.id}>
+            {item.name}
+          </div>
+        )) }
+      </div>
     </div>
   );
 }
